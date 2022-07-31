2cf7d14e2d364573e5883263c84a57a3c0000e04
Leonardo HP <ba.leonardohp@gmail.com>
2022-07-31 12:27:47 -0500

    Primer cotizador

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+// Los accesos directos ayudan a simplificar Mi unidad … 
+// En las próximas semanas, los elementos que se encuentren en más de una carpeta se reemplazarán por accesos directos. El acceso a los archivos y las carpetas no cambiará.Más información
+const selectZonaVivienda = document.querySelector("#selectZonaVivienda")
+const selectTipoVivienda = document.querySelector("#selectTipoVivienda")
+
+const metros2 = document.querySelector("#metros2")
+const btnCotizar = document.querySelector("#btnCotizar")
+const btnRefrescar = document.querySelector("#btnRefrescar")
+const valorCuota = document.querySelector("#valorCuota")
