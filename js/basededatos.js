8f0c4871998ffd761a6c8191a24eb5c2c9f8c016
Leonardo HP <ba.leonardohp@gmail.com>
2022-07-31 16:03:21 -0500

    -

--- js/basededatos.js
+++ js/basededatos.js
@@ -1,11 +1,8 @@
-
-// Los accesos directos ayudan a simplificar Mi unidad … 
-// En las próximas semanas, los elementos que se encuentren en más de una carpeta se reemplazarán por accesos directos. El acceso a los archivos y las carpetas no cambiará.Más información
 const tipoVivienda = [{"tipo": "Casa", "factor": 1.05},
                       {"tipo": "P.H.", "factor": 1.01},
                       {"tipo": "Edificio Departamento", "factor": 1.00},
-                      {"tipo": "Barrio Privado", "factor": 1.19},]
-                      //{"tipo": "Local Comercial", "factor": 2.91}
+                      {"tipo": "Barrio Privado", "factor": 1.19},
+                      {"tipo": "Local Comercial", "factor": 2.91}]
 
 const zonaVivienda = [{"area": "CABA", "factor": 1.02}, 
                       {"area": "La Plata", "factor": 1.03},
